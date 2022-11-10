c0ec68374e90242baf4885fedf16a12019b856c8
Anjanay Raina <anjanayraina326@gmail.com>
2022-11-11 00:29:56 +0530

    external and calldata added

--- LoadingStateVariables.sol
+++ LoadingStateVariables.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.0;
 contract LoadingStateVariables{
     uint public total;
-    
+    // [1,2,3,4,5,6]
     // gas cost : 50012
     function sumIsEvenMoreGasStart(uint[] memory a) public {
         for(uint i=0;i<a.length;i+=1){
@@ -11,6 +11,17 @@ contract LoadingStateVariables{
         }
 
 
+    }
+    // gas cost : 48954
+    // reasons for the gas saved : 
+    // 1) memory has been changed to calldata as we are just reading the array and calldata saves gas
+    // 2) external has been used as we know that it will not be called from inside the contract , this saves some gas as well
+    function moreGasSaved(uint[] calldata a)external {
+        for(uint i=0;i<a.length;i++){
+            if(a[i]%2 == 0 && a[i] < 99){
+                total+=a[i];
+            }
+        }
     }
 
 }
